32ad38c4c1de4b88967b6a53b992c2c17b18cc1e
Onur A. Karakum <113251720+oakarakum@users.noreply.github.com>
2022-12-31 16:40:08 +0300

    bottom nav bar tamamlandı renk bekleniyor.

--- lib/app/controllers/home_controller.dart
+++ lib/app/controllers/home_controller.dart
@@ -1,7 +1,14 @@
-
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:responsive_sizer/responsive_sizer.dart';
 
 class HomeController extends GetxController {
-
+  RxList<Widget> items = [
+    Icon(Icons.calendar_month, size: 4.h),
+    Icon(Icons.map, size: 4.h),
+    Icon(Icons.home, size: 4.h),
+    Icon(Icons.bus_alert, size: 4.h),
+    Icon(Icons.calculate, size: 4.h),
+  ].obs;
+  RxInt index = 2.obs;
 }
-  
--- lib/app/ui/pages/home_page.dart
+++ lib/app/ui/pages/home_page.dart
@@ -1,30 +1,60 @@
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 import 'package:responsive_sizer/responsive_sizer.dart';
 import '../../controllers/home_controller.dart';
 
-
 class HomePage extends GetView<HomeController> {
+  const HomePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      resizeToAvoidBottomInset: false,
+        resizeToAvoidBottomInset: false,
         appBar: AppBar(
-          
-          
+          toolbarHeight: 0,
           backgroundColor: Colors.white,
         ),
-      body: Padding(
-        padding:  EdgeInsets.symmetric(horizontal: 5.w),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const [
-            
-          ],
+        bottomNavigationBar: CurvedNavigationBar(
+          height: 57,
+          items: controller.items,
+          index: controller.index.value,
+          onTap: (value) {
+            controller.index.value = value;
+          },
+          backgroundColor: Colors.deepOrange,
+          color: Colors.pink,
         ),
-      )
-    );
+        body: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 5.w),
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 1.h),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    SizedBox(
+                        height: 2.6.h,
+                        width: 10.w,
+                        child: Image.asset(
+                          "assets/menu-bar.png",
+                        )),
+                    SizedBox(width: 25.w),
+                    SizedBox(width: 30.w),
+                    Icon(
+                      Icons.settings,
+                      color: Colors.black,
+                      size: 3.4.h,
+                    )
+                  ],
+                ),
+              )
+            ],
+          ),
+        ));
   }
 }
-  
